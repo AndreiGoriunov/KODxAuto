7ef8d75ed26cd3f05fc81f5e3a429469bc9c7a97
AndreiGoriunov <52248218+AndreiGoriunov@users.noreply.github.com>
2022-11-14 20:25:13 -0500

    Issue #1 fix.

--- kodxauto/kodxauto.py
+++ kodxauto/kodxauto.py
@@ -6,18 +6,16 @@ from time import perf_counter
 from .executor import Executor
 from .step_handler import StepParser
 
-true_options = ("1", "true", "True")
-
 
 class KODxAuto:
     """KODXAuto automation class."""
 
     def __init__(self):
-        self.config = {}
+        self.properties = {}
         self.root_abs_path = ""
 
     def __str__(self):
-        return f"config = {self.config}, root_abs_path = {self.root_abs_path}"
+        return f"properties = {self.properties}, root_abs_path = {self.root_abs_path}"
 
     def set_root_dir(self, root_dir: str) -> None:
         """Required. Set a directory where the `kodxauto.properties` is located.
@@ -34,7 +32,7 @@ class KODxAuto:
         self.__set_logger()
 
     def set_properties(self, **props) -> None:
-        """Optional. Used to set or overwrite config file properties.
+        """Optional. Used to set or overwrite properties file properties.
         Available properties:
         - macros_folder_path (str)
         - log_file_path (str)
@@ -42,30 +40,47 @@ class KODxAuto:
         - macro_name (str)
         """
         for key, value in props.items():
-            self.config[key] = value
+            self.properties[key] = value
+
+    def get_property(self, property_name: str) -> str | bool:
+        """Getter for properies."""
+        true_options = ("1", "true", "True")
+        false_options = ("0", "false", "False")
+        if not self.properties:
+            _error = "self.properties is empty. No properties were set."
+            logging.error(_error)
+            raise Exception(_error)
+        property_value = None
+        try:
+            property_value = self.properties[property_name]
+        except KeyError:
+            _error = f'There is no propery with the key "{property_name}" set.'
+            logging.error(_error)
+            raise KeyError(_error)
+
+        if property_value in true_options:
+            return True
+        elif property_value in false_options:
+            return False
+        elif property_value == "":
+            return None
+        else:
+            return str(property_value)
 
     def run(self, macro_name=None):
-        if not macro_name:
-            if ("macro_name" not in self.config) or (self.config["macro_name"] == ""):
-                logging.error(
-                    "macro_name is not defined. Define macro_name in kodxauto.properties or pass it as an argument."
-                )
-                raise Exception(
-                    "macro_name is not defined. Define macro_name in kodxauto.properties or pass it as an argument."
-                )
-            else:
-                macro_name = self.config["macro_name"]
+        macro_name = self.get_property("macro_name")
 
         # Path to the macros directory containing steps
         _path = os.path.join(
-            self.root_abs_path, self.config["macros_folder_path"], macro_name
+            self.root_abs_path, self.get_property("macros_folder_path"), macro_name
         )
 
-        # Check "recompile" config property
+        # Check "recompile" property
+        recompile = self.get_property("recompile")
+
+        # If property is set to true or not set at all then recompile
         bf_parse = perf_counter()  # timer before parse/read
-        if (self.config["recompile"] != "") and self.config[
-            "recompile"
-        ] in true_options:
+        if recompile is None or recompile:
             steps = StepParser(_path).parse_steps()
             logging.info(f"Parsed steps: {steps}")
         else:
@@ -85,19 +100,18 @@ class KODxAuto:
 
     def __set_config_file(self):
         if not os.path.exists(self.config_path):
-            raise FileNotFoundError(
-                f"kodxauto.properties was not found on the given path: {self.config_path}"
-            )
+            _error = f"kodxauto.properties was not found on the given path: {self.config_path}"
+            raise FileNotFoundError(_error)
 
-        # get config file and store it in self.config dictionary
+        # get properties file and store it in self.properties dictionary
         _config_parser = ConfigParser()
         _config_parser.read(self.config_path)
-        self.config = dict(_config_parser.items("settings"))
+        self.properties = dict(_config_parser.items("settings"))
 
     def __set_logger(self):
         """Initialize the logger"""
         logging.basicConfig(
-            filename=self.config["log_file_path"],
+            filename=self.get_property("log_file_path"),
             filemode="w",
             level=logging.INFO,
             encoding="utf-8",
